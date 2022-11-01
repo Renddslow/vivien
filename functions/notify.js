1dfc52f1e54bf61860ef8a440db0590adf2b42d1
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2022-10-31 22:21:29 -0500

    feat: inital commit

---
+++ functions/notify.js
@@ -0,0 +1,17 @@
+const twilio = require('twilio');
+
+const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+
+exports.handler = async (event, context) => {
+    const { headcount, family } = JSON.parse(event.body);
+
+    await client.messages.create({
+        body: `The ${family} family is coming to Rowan's party! They are bringing ${headcount} guests.`,
+        from: '+14023789202',
+        to: process.env.PHONE_NUMBER,
+    });
+
+    return {
+        statusCode: 200,
+    };
+}
