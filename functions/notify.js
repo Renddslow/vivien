1be272f72e5038541640914cc16cd075fc552585
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2023-10-27 23:25:34 -0500

    feat: updated site for Vivi's 4th

--- functions/notify.js
+++ functions/notify.js
@@ -1,17 +1,20 @@
-const twilio = require('twilio');
-
-const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH);
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event, context) => {
-    const { headcount, family } = JSON.parse(event.body);
+  const { headcount, family } = JSON.parse(event.body);
 
-    await client.messages.create({
-        body: `The ${family} family is coming to Vivien's party! They are bringing ${headcount} guests.`,
-        from: '+14023789202',
-        to: process.env.PHONE_NUMBER,
-    });
+  const msg = {
+    to: "mubatt@wyopub.com",
+    from: "hello@vivienmcelwee.com", // Use the email address or domain you verified above
+    subject: `The ${family} family is coming to Vivien's party!`,
+    text: `The ${family} family is coming to Vivien's party! They are bringing ${headcount} guests.`,
+    html: `<p>The ${family} family is coming to Vivien's party! They are bringing ${headcount} guests.</p>`,
+  };
 
-    return {
-        statusCode: 200,
-    };
-}
+  await sgMail.send(msg);
+
+  return {
+    statusCode: 200,
+  };
+};
